4072a48b98aa850a39db2404c5659cabde1e95d0
Leonardo Testi <ltesti120a@gmail.com>
2016-06-10 11:28:43 +0200

    Extract uvdata

--- extract_uvtables.py
+++ extract_uvtables.py
@@ -8,14 +8,19 @@ datadir = '../data_fits/'
 
 tab = aio.read('table_wizard.txt')
 
+datafiles_orig = []
 datafiles = []
 outfiles =[]
 for src in tab:
-    datafiles.append(datadir+src['Name']+'_selfcal.ms')
+    datafiles_orig.append(datadir+src['Name']+'_selfcal.ms')
+    datafiles.append(datadir+src['Name']+'_selfcal_now0.ms')
     outfiles.append('../uvdata/alma_b7_'+src['ID']+'.txt')
 
 
 for i in range(len(datafiles)):
+    #
+    # split out removing w=0 data
+    split(vis=datafiles_orig[i],outputvis=datafiles[i],datacolumn='data',keepflags=False)
 	print("--> Importing ms table: {0}".format(datafiles[i]))
 	myuvdata = UVDataMS("dummy", (datafiles[i], tb))
 	rat_re,rat_im = myuvdata.get_weight()
